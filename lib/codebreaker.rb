bc28895de7dc1f1106123ee57702ce90e2de2548
Ajiexwel4 <ajiexwel4@gmail.com>
2016-12-17 19:20:09 +0200

    View of score

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,3 +1,5 @@
 require_relative 'codebreaker/version'
 require_relative 'codebreaker/game'
 require_relative 'codebreaker/console_game'
+
+Codebreaker::ConsoleGame.new.play
--- lib/codebreaker/modules/score.rb
+++ lib/codebreaker/modules/score.rb
@@ -7,10 +7,11 @@ module Codebreaker
     end
 
     private
+
     def save_score_file
       name = input_name
-      path = File.expand_path("../../score/#{name}_score.txt", __FILE__)
-      File.open(path, 'a') { |file| file.puts "#{@game.score} - #{Time.now.asctime}" }
+      path = File.expand_path('../../score/score.txt', __FILE__)
+      File.open(path, 'a') { |file| file.puts "#{name}: #{@game.score} score on #{Time.now.asctime}" }
       puts "You score saved in file: #{path}"
     end
   end
